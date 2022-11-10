7b0f1aa518dbf7193cff9a7c7ecedab25ad72b92
SemCaputo <109221534+SemCaputo@users.noreply.github.com>
2022-11-10 19:44:51 +0000

    C++ coursework 1st lesson

--- test1.cpp
+++ test1.cpp
@@ -2,4 +2,35 @@
 
 using namespace std;
 
-int lenght;
+// int main()
+// {
+//     int lenght = 0;
+//     int width = 0;
+//     int Area = 0;
+
+//     lenght = 7;
+//     width = 5;
+
+//     Area = lenght * width;
+
+//     cout << "The Area calculated is";
+//     cout << Area;
+
+//     return 0;
+// }
+
+int main()
+{
+    int lenght = 0;
+    int width = 0;
+    int Area = 0;
+
+    lenght = 6;
+    width = 6;
+
+    cout << "Input Lenght";
+    cin >> lenght;
+
+    cout << "Input Width";
+    cin >> width;
+}
